003a9e65b545a7d3578a2f78b5f49312373704a5
CodeLearningMagic <91286803+CodeLearningMagic@users.noreply.github.com>
2022-04-25 17:54:37 -0500

    Add files via upload

---
+++ PROC37 CORRECTO/js/Form.js
@@ -0,0 +1,24 @@
+class Form {
+  constructor() {
+    
+  }
+
+  setElementsPosition() {
+   
+  }
+
+ 
+
+  hide() {
+    
+  }
+
+  handleMousePressed() {
+    
+  }
+
+  display() {
+   
+  }
+  
+}
